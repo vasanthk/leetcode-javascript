5b43fb889b767fe7de006b97e3ce4b5f079778d7
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-20 14:17:09 -0800

    Valid Palindrome

---
+++ 125.Valid Palindrome.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ *
+ * Your runtime beats 94.49% of javascriptsubmissions.
+ */
+var isPalindrome = function (s) {
+  s = s.replace(/\W/g, '');
+  if (s === '') {
+    return true;
+  }
+
+  s = s.toLowerCase();
+  for (var i = 0, j = s.length - 1; i < j; i++, j--) {
+    if (s.charAt(i) !== s.charAt(j)) {
+      return false;
+    }
+  }
+  return true;
+};
