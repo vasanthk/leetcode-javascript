d1df288cada0066c47d8739b858b5542a6a497f6
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-27 16:12:24 -0800

    Renamed file

--- 155.Min Stack.js
+++ 155.Min Stack.js
@@ -1,5 +1,10 @@
 /**
- * @constructor
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ *
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
  *
  * Your runtime beats 96.43% of javascriptsubmissions.
  */
@@ -46,6 +51,6 @@ MinStack.prototype.top = function () {
  */
 MinStack.prototype.getMin = function () {
   return this.min[this.min.length - 1];
-  // Caveat: If the first element was the min and it was popped, the array woudl be empty.
+  // Caveat: If the first element was the min and it was popped, the array would be empty.
   // But the question doesn't mention that scenario, so we are good for now.
 };
