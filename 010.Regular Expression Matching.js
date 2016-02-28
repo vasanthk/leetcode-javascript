01f30dbeba21b87dc44109e0e98235f60faedf57
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-28 00:27:26 -0800

    Added problem info

--- 010.Regular Expression Matching.js
+++ 010.Regular Expression Matching.js
@@ -1,8 +1,24 @@
 /**
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ *
+ * The function prototype should be: bool isMatch(const char *s, const char *p)
+ *
+ * Some examples:
+ * isMatch("aa","a") → false
+ * isMatch("aa","aa") → true
+ * isMatch("aaa","aa") → false
+ * isMatch("aa", "a*") → true
+ * isMatch("aa", ".*") → true
+ * isMatch("ab", ".*") → true
+ * isMatch("aab", "c*a*b") → true
+ *
  * @param {string} s
  * @param {string} p
  * @return {boolean}
  */
+
 var isMatch = function (s, p) {
   if (p.length === 0) {
     return s.length === 0;
